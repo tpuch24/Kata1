7d8a8e488ee6fa4109d901e62b9160299eb15bb5
Thierry Puch <t.puch@laposte.net>
2016-05-24 14:05:20 +0200

    After test zeroForNullString

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,20 @@
-
+/**
+ * 
+ * @author Thierry
+ *
+ * Class
+ */
 public class StringCalculator {
 
+	public StringCalculator() {
+		super();
+
+	}
+
+	public int add(String numbers){
+		
+		if (numbers.length()==0) return 0;
+		
+		return 1;
+	}
 }
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,4 +1,24 @@
+/**
+ * 
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
 
 public class StringCalculatorTest {
 
+	@Test
+	public void nullValueForEmptyStringTest(){
+		
+		StringCalculator strcalc = new StringCalculator();
+		
+		int retVal = strcalc.add("");
+		
+		assertEquals(0, retVal);
+		
+		
+	}
 }
