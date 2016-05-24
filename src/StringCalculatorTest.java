778c56e337c7233d468119985699702c5ff980dd
Thierry Puch <t.puch@laposte.net>
2016-05-24 14:47:34 +0200

    After test 2 values

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -4,28 +4,37 @@
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 
 public class StringCalculatorTest {
-
+	
+	private static final String TWO_INT_VALUES_STRING = "1,2";
+	private static final String ONE_INT_VALUE_STRING = "8";
+	private static final String EMPTY_STRING = "";
+	private StringCalculator stringCalculator;
+	
+	@Before
+	public void init(){
+		stringCalculator = new StringCalculator();
+	}
 	@Test
 	public void nullValueForEmptyStringTest(){
 		
-		StringCalculator strcalc = new StringCalculator();
-		
-		int retVal = strcalc.add("");
-		
-		assertEquals(0, retVal);
+		assertEquals(0, stringCalculator.add(EMPTY_STRING));
 	}
+	
 	@Test
 	public void OneValueForOneStringTest(){
 		
-		StringCalculator strcalc = new StringCalculator();
+		assertEquals(8, stringCalculator.add(ONE_INT_VALUE_STRING));
+	}
+	
+	@Test
+	public void ValueForTwoIntStringTest(){
 		
-		int retVal = strcalc.add("1");
-		
-		assertEquals(1, retVal);
+		assertEquals(3, stringCalculator.add(TWO_INT_VALUES_STRING));
 	}
 }
